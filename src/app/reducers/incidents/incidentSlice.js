97d3d8437332aefb8fd910f3cdbcd2f4981b4cf2
Christine Martino <christine.martino@thinkcompany.com>
2021-04-21 21:40:56 -0400

    FIx fetch request

--- src/app/reducers/incidents/incidentSlice.js
+++ src/app/reducers/incidents/incidentSlice.js
@@ -1,71 +1,75 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // import CaseDetails from '../../../features/incidents/CaseDetails';
 
-export const fetchIncidents = createAsyncThunk('incidents/fetchIncidents', async () => {
-    const response = fetch(`${process.env.PORT || 'http://localhost:8080'}/incidents/get-incidents-db`)
-    // downloaded a fetch polyfill 
-    .then(function(response) {
+export const fetchIncidents = createAsyncThunk(
+  "incidents/fetchIncidents",
+  async () => {
+    const response = fetch(`/incidents/get-incidents-db`)
+      // downloaded a fetch polyfill
+      .then(function (response) {
         if (!response.ok) {
-            return response.statusText
+          return response.statusText;
         }
         return response.json();
-    })
-    return response
-});
+      });
+    return response;
+  }
+);
 
 export const incidentSlice = createSlice({
-    name: 'incident',
-    initialState: {
-        meta: {
-            status: 'idle',
-            fetch: true,
-            errorLog: {
-                error: false,
-                message: '',
-                errorCode: '',
-            }
-        },
-        data: {
-            selectedIncidentId: '',
-            incidentList: [],
-            selectedCaseId: '',
-        }
+  name: "incident",
+  initialState: {
+    meta: {
+      status: "idle",
+      fetch: true,
+      errorLog: {
+        error: false,
+        message: "",
+        errorCode: "",
+      },
     },
-    reducers:{
-        select: (state, {payload}) => {
-            state.data.selectedIncidentId = payload;
-        },
-        selectCase: (state, {payload}) => {
-            state.data.selectedCaseId = payload;
-        },
-        updateList: (state, {payload}) => {
-            state.data.incidentList = payload;
-        },
+    data: {
+      selectedIncidentId: "",
+      incidentList: [],
+      selectedCaseId: "",
     },
-    extraReducers: {
-        [fetchIncidents.pending]: (state) => {
-            state.meta.status = 'loading';
-        },
-        [fetchIncidents.fulfilled]: (state, action) => {
-            state.meta.status = 'success';
-            state.data.incidentList = action.payload;
-            // Need to figure out how to go about refreshing data 
-        },
-        [fetchIncidents.rejected]: (state, action) => {
-            state.meta.status = 'failed';
-            state.meta.error = action.error.message
-            // state.data.incidentList = payload;
-        },
+  },
+  reducers: {
+    select: (state, { payload }) => {
+      state.data.selectedIncidentId = payload;
     },
+    selectCase: (state, { payload }) => {
+      state.data.selectedCaseId = payload;
+    },
+    updateList: (state, { payload }) => {
+      state.data.incidentList = payload;
+    },
+  },
+  extraReducers: {
+    [fetchIncidents.pending]: (state) => {
+      state.meta.status = "loading";
+    },
+    [fetchIncidents.fulfilled]: (state, action) => {
+      state.meta.status = "success";
+      state.data.incidentList = action.payload;
+      // Need to figure out how to go about refreshing data
+    },
+    [fetchIncidents.rejected]: (state, action) => {
+      state.meta.status = "failed";
+      state.meta.error = action.error.message;
+      // state.data.incidentList = payload;
+    },
+  },
 });
 
 export const { select, selectCase, updateList } = incidentSlice.actions;
 
-export const selectIncident = state => state.incidents.data.selectedIncidentId;
-export const selectCaseId = state => state.incidents.data.selectedCaseId;
-export const listIncidents = state => state.incidents.data.incidentList;
-export const errorLog = state => state.incidents.meta.errorLog;
-export const refreshState = state => state.incidents.meta.refresh;
+export const selectIncident = (state) =>
+  state.incidents.data.selectedIncidentId;
+export const selectCaseId = (state) => state.incidents.data.selectedCaseId;
+export const listIncidents = (state) => state.incidents.data.incidentList;
+export const errorLog = (state) => state.incidents.meta.errorLog;
+export const refreshState = (state) => state.incidents.meta.refresh;
 // For getting the value of the current state throughout the app;
 
 // Formatter Function for preparing selectedIncident Data for DetailsBlock
@@ -73,98 +77,138 @@ export const refreshState = state => state.incidents.meta.refresh;
 // Transformation and formatting of data makes more sense in reducer middleware than the React component. (seg: Smart vs Dumb components)
 
 // export const selectedIncident = incidents.find((incident) => !incident._id.indexOf(selectedId));
-export const getSelectedIncident = (state) => state.incidents.data.incidentList.find((incident) => !incident._id.indexOf(selectIncident));
+export const getSelectedIncident = (state) =>
+  state.incidents.data.incidentList.find(
+    (incident) => !incident._id.indexOf(selectIncident)
+  );
 export const getSelectedCase = (state) => {
-    const selectedId = selectIncident(state);
-    const incidentList = listIncidents(state);
-    const selectedIncident = incidentList.find((incident) => !incident._id.indexOf(selectedId));
+  const selectedId = selectIncident(state);
+  const incidentList = listIncidents(state);
+  const selectedIncident = incidentList.find(
+    (incident) => !incident._id.indexOf(selectedId)
+  );
 
-    if (!selectedIncident) { return {}; }
+  if (!selectedIncident) {
+    return {};
+  }
 
-    const caseList = selectedIncident.incident.cases
-    const caseId = selectCaseId(state);
-    const selectedCase = caseList.find((cases)=> !cases.zip_code.indexOf(caseId));
+  const caseList = selectedIncident.incident.cases;
+  const caseId = selectCaseId(state);
+  const selectedCase = caseList.find(
+    (cases) => !cases.zip_code.indexOf(caseId)
+  );
 
-    return selectedCase;
-}
+  return selectedCase;
+};
 
 export const getIncidentDetailBlocks = (state) => {
-    const selectedId = selectIncident(state); // Reusing selectors above
-    const incidentList = listIncidents(state); // Reusing selectors above
-    const selectedIncident = incidentList.find((incident) => !incident._id.indexOf(selectedId));
+  const selectedId = selectIncident(state); // Reusing selectors above
+  const incidentList = listIncidents(state); // Reusing selectors above
+  const selectedIncident = incidentList.find(
+    (incident) => !incident._id.indexOf(selectedId)
+  );
 
-    if (!selectedIncident) { return {}; }
-    // console.log(selectedIncident, 'iuncident')
-    const { geographics, incident } = selectedIncident;
-    // const cases = getSelectedCase(state);
-    // console.log(cases)
+  if (!selectedIncident) {
+    return {};
+  }
+  // console.log(selectedIncident, 'iuncident')
+  const { geographics, incident } = selectedIncident;
+  // const cases = getSelectedCase(state);
+  // console.log(cases)
 
-    // Set up the data representation of our detail blocks.
-    // This keeps the data formatting responsibility separate from presentational components.
-    // We can break this down further if we wanted to with each "block" having its own formatter function outputting the correct object structure, then adding it to the array.
-    const blocks =  [
+  // Set up the data representation of our detail blocks.
+  // This keeps the data formatting responsibility separate from presentational components.
+  // We can break this down further if we wanted to with each "block" having its own formatter function outputting the correct object structure, then adding it to the array.
+  const blocks = [
+    {
+      incidentDetails: [
         {
-            incidentDetails: [
-                {
-                    title: 'Incident Information',
-                    rows:  [
-                        { type: 'Incident', content: geographics.municipal },
-                        { type: 'State', content: geographics.state }, 
-                        { type: 'Region', content: geographics.region }, 
-                        { type: 'ID', content: selectedIncident._id.substr(selectedId.length - 5) }, 
-                        { type: 'Initial Time', content: `${geographics.time_stamp} ${geographics.time_zone} `}, 
-                        { type: 'Zipcodes Affected', content: incident.zip_codes.map(zip => `${zip}, `) }
-                    ] 
-                },
-                {
-                    title: 'Areas Affected',
-                    rows:  [
-                        { type: 'Volume Traffic', content: incident.volume_traffic },
-                        { type: 'Api Keywords', content: incident.api_keywords.map(keyword => `${keyword}, `)},
-                        { type: 'Property', content: incident.property.map(property => `${property}, `) },
-                        { type: 'Wildfire Type', content: incident.wildfire_type },
-                        { type: 'Priority', content: incident.priority },
-                    ] 
-                },
-            ]
+          title: "Incident Information",
+          rows: [
+            { type: "Incident", content: geographics.municipal },
+            { type: "State", content: geographics.state },
+            { type: "Region", content: geographics.region },
+            {
+              type: "ID",
+              content: selectedIncident._id.substr(selectedId.length - 5),
+            },
+            {
+              type: "Initial Time",
+              content: `${geographics.time_stamp} ${geographics.time_zone} `,
+            },
+            {
+              type: "Zipcodes Affected",
+              content: incident.zip_codes.map((zip) => `${zip}, `),
+            },
+          ],
         },
-    ];
+        {
+          title: "Areas Affected",
+          rows: [
+            { type: "Volume Traffic", content: incident.volume_traffic },
+            {
+              type: "Api Keywords",
+              content: incident.api_keywords.map((keyword) => `${keyword}, `),
+            },
+            {
+              type: "Property",
+              content: incident.property.map((property) => `${property}, `),
+            },
+            { type: "Wildfire Type", content: incident.wildfire_type },
+            { type: "Priority", content: incident.priority },
+          ],
+        },
+      ],
+    },
+  ];
 
-    return blocks || [];
+  return blocks || [];
 };
 
 export const getCaseDetailBlocks = (state) => {
-    const selectedId = selectIncident(state); // Reusing selectors above
-    const incidentList = listIncidents(state); // Reusing selectors above
-    const selectedIncident = incidentList.find((incident) => !incident._id.indexOf(selectedId));
-    const cases = getSelectedCase(state);
+  const selectedId = selectIncident(state); // Reusing selectors above
+  const incidentList = listIncidents(state); // Reusing selectors above
+  const selectedIncident = incidentList.find(
+    (incident) => !incident._id.indexOf(selectedId)
+  );
+  const cases = getSelectedCase(state);
 
-    if (!selectedIncident && cases) { return {}; }
-    const { geographics, incident } = selectedIncident;
+  if (!selectedIncident && cases) {
+    return {};
+  }
+  const { geographics, incident } = selectedIncident;
 
-    const blocks =  [
+  const blocks = [
+    {
+      caseDetails: [
         {
-            caseDetails: [
-                {
-                    title: 'Case Information',
-                    rows:  [
-                        { type: 'Incident', content: geographics.municipal },
-                        { type: 'State', content: geographics.state }, 
-                        { type: 'Region', content: geographics.region }, 
-                        { type: 'ID', content: selectedIncident._id.substr(selectedId.length - 5) }, 
-                        { type: 'Initial Time', content: `${cases.initial_time} ${geographics.time_zone} `}, 
-                        { type: 'Api Keywords', content: incident.api_keywords.map(keyword => `${keyword} ,`)},
-                        { type: 'Authorities Present', content: cases.authorities_present}
-                    ]
-                },
-                {
-                    title: 'Images',
-                    rows: cases.images
-                },
-            ]
-        }
-    ];
-    return blocks || [];
-}
+          title: "Case Information",
+          rows: [
+            { type: "Incident", content: geographics.municipal },
+            { type: "State", content: geographics.state },
+            { type: "Region", content: geographics.region },
+            {
+              type: "ID",
+              content: selectedIncident._id.substr(selectedId.length - 5),
+            },
+            {
+              type: "Initial Time",
+              content: `${cases.initial_time} ${geographics.time_zone} `,
+            },
+            {
+              type: "Api Keywords",
+              content: incident.api_keywords.map((keyword) => `${keyword} ,`),
+            },
+            { type: "Authorities Present", content: cases.authorities_present },
+          ],
+        },
+        {
+          title: "Images",
+          rows: cases.images,
+        },
+      ],
+    },
+  ];
+  return blocks || [];
+};
 export default incidentSlice.reducer;
-
